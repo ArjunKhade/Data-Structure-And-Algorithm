7e0577d555de8a1b94576ef9d4ac596ac0da84f1
ArjunKhade <khadearjun@gmail.com>
2026-02-18 10:33:05 +0530

    completed the programming 101 variables, array, object

---
+++ index.js
@@ -0,0 +1,21 @@
+//variables 
+const a = 10;
+const b = 20;
+const sum = a+b;
+console.log(sum);
+
+//arrays 
+let arr = [1, 2, [3, 4], "Arjun", true, "Monika"];
+console.log(arr);
+console.log(arr[2][1]);
+
+//object
+let obj = {
+    a: 7,
+    name: "Arjun",
+    bool: true,
+    arr: [6, 7, 8]
+}
+
+console.log(obj);
+console.log(obj.name);
