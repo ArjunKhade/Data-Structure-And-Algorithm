06f90d43bb383d3f5219515f3acafe1afcf017ed
ArjunKhade <khadearjun@gmail.com>
2026-03-08 18:05:12 +0530

    created function to merge two sorted array in place.

--- array-easy-medium/mergeSortedArray.js
+++ array-easy-medium/mergeSortedArray.js
@@ -3,7 +3,8 @@ let m = 3;
 const nums2 = [2,5,6]
 let n = 3;
 
-mergeSortedArray(nums1,m,nums2,n);
+// mergeSortedArray(nums1,m,nums2,n);
+mergeSortedArray1(nums1,m,nums2,n)
 console.log(nums1);
 
 
@@ -28,3 +29,44 @@ function mergeSortedArray(nums1,m,nums2,n) {
        }
     }
 }
+
+//Approch 2 (in place)
+
+function mergeSortedArray1(nums1, m, nums2, n){
+
+   // Pointer for the last valid element in nums1 and nums2
+   let p1 = m - 1;
+   let p2 = n - 1;
+
+   // Start filling nums1 from the last index
+   for(let i = m + n - 1; i >= 0; i--){
+
+      // If all elements of nums2 are already placed, stop
+      if(p2 < 0) break;
+
+      // Compare elements from the end of nums1 and nums2
+      // Place the larger element at the current index i
+      if(p1 >= 0 && nums1[p1] > nums2[p2]){
+         
+         // Move element from nums1 to position i
+         nums1[i] = nums1[p1];
+
+         // Move nums1 pointer backward
+         p1--;
+
+      }else{
+
+         // Move element from nums2 to position i
+         nums1[i] = nums2[p2];
+
+         // Move nums2 pointer backward
+         p2--;
+      }
+   }
+}
+
+
+
+
+
+
