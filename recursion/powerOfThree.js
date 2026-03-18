ce5bfa201e77c474d99107b2724785e5029147bd
monika131001 <monikasavhad78@gmail.com>
2026-03-18 10:33:51 +0530

    created function to check if given number is power of three

---
+++ recursion/powerOfThree.js
@@ -0,0 +1,10 @@
+
+console.log(isPowerOfThree(9));
+
+
+function isPowerOfThree(n){
+    if(n <= 0) return false;
+    if(n == 1) return true;
+    if(n % 3 !== 0) return false;
+    return isPowerOfThree(n/3);
+}
