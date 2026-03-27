7d8a9714a7798ec31aff0444bdeb6b1a8ea89b9a
ArjunKhade <khadearjun@gmail.com>
2026-03-27 10:24:13 +0530

    implemented function for bubble sort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,4 +1,24 @@
 
-function bubbleSort(){
-    
+let arr = [4,5,1,3,9];
+
+console.log(bubbleSort(arr));
+
+function bubbleSort(arr){
+    let n = arr.length;
+    for(let i=0; i<n-1; i++){
+        let isSwapped = false;
+        for(let j=0; j<n-1-i; j++){
+           if(arr[j]>arr[j+1]){
+            //swap
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1]= temp;
+            isSwapped = true;
+           } 
+        }
+        if(!isSwapped){
+            break;
+        }
+    }
+    return arr;
 }
