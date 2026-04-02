4f61d8a3a13f605466aaf496a8b098d6c17f6296
ArjunKhade <khadearjun@gmail.com>
2026-04-02 22:27:11 +0530

    created function to check valid palindrome in place

--- string/validPalindrome.js
+++ string/validPalindrome.js
@@ -1,6 +1,7 @@
 let s = "A man, a plan, a canal: Panama";
 
 console.log(validPalindrome(s));
+console.log(validPalindrome1(s))
 
 // removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters
 // include letters and numbers.
@@ -17,4 +18,27 @@ function validPalindrome(s){
         }
     }
     return rev === filteredString;
+}
+
+
+//Aprroach 2 In place no extra space
+function validPalindrome1(s){
+  s = s.toLowerCase();
+  let i = 0;
+  let j = s.length-1;
+
+  while(i<j){
+    if(!s[i].match(/[a-z0-9]/i)){
+        i++;
+    }else if(!s[j].match(/[a-z0-9]/i)) {
+       j--;
+    }else if (s[i] === s[j]){
+        i++;
+        j--;
+    }else{
+        return false;
+    }
+  }
+
+return true;
 }
