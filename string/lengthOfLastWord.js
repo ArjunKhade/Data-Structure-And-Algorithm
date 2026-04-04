e1209c33dd9ec173c501ac402a649c76dd979029
ArjunKhade <khadearjun@gmail.com>
2026-04-04 11:59:51 +0530

    created function to find out length of last word.

--- string/lengthOfLastWord.js
+++ string/lengthOfLastWord.js
@@ -1,6 +1,8 @@
 let s = "   fly me   to   the moon  ";
 
 console.log(lengthOfLastWord(s));
+console.log(lengthOfLastWord1(s));
+
 
 function lengthOfLastWord(s) {
   s = s.toLowerCase();
@@ -17,4 +19,21 @@ function lengthOfLastWord(s) {
   }
 
   return count;
+}
+
+
+//Approch 2 Single loop
+
+function lengthOfLastWord1(s){
+  let index = s.length - 1;
+  let count = 0;
+  while(index>=0){
+     if(s[index] !== " "){
+      ++count;
+     }else if(count>0){
+       break;
+     }
+    --index;
+  }
+   return count;
 }
