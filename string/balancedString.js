8b5a709ff1027ac0df3a9565ebd03d8a8c3d5cda
ArjunKhade <khadearjun@gmail.com>
2026-04-08 10:09:59 +0530

    created function to check if string is balanced or not

---
+++ string/balancedString.js
@@ -0,0 +1,20 @@
+
+let s = "RLRRLLRLRL"
+// Output: 4
+
+console.log(balancedString(s));
+
+function balancedString(s){
+    let temp = 0;
+    let count = 0;
+    for(let i=0; i<s.length; i++){
+        if(s[i] === 'R'){
+          ++temp;
+        }else{
+            --temp;
+        }
+
+        if(temp === 0) ++count;
+    }
+    return count;
+}
