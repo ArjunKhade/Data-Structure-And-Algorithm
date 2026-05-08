dfa3279d46daad0cc2133aed2ad9bdba8884e25b
ArjunKhade <khadearjun@gmail.com>
2026-05-08 08:57:42 +0530

    find out middle of linked list

---
+++ linked-list/middleOfLinkedList.js
@@ -0,0 +1,13 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function middleNode (head) {
+    let slow = head;
+    let fast = head;
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+};
