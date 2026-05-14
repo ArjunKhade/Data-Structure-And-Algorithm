e0b622cf6fe98b694edde68afcfeb0051a46558e
ArjunKhade <khadearjun@gmail.com>
2026-05-14 11:06:32 +0530

    created function to find out intersection of linked list.

---
+++ linked-list/getIntersectionNode.js
@@ -0,0 +1,21 @@
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let set  = new Set();
+
+    while(headB){
+        set.add(headB);
+        headB = headB.next;
+    }
+
+    while(headA){
+        if(set.has(headA)) return headA;
+        headA = headA.next;
+    }
+
+    return null;
+};
